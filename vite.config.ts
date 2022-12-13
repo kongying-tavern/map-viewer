05adc6211a22ea7bd2413515efe6b831e0c5d10b
peaceshi <peaceshi@outlook.com>
2022-12-13 23:49:30 +0800

    remove pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,84 +1,84 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: "prompt",
-      includeManifestIcons: false,
-      devOptions: {
-        enabled: false,
-      },
-      manifest: {
-        id: "/",
-        name: "Map Viewer",
-        short_name: "MapViewer",
-        description: "Map Viewer",
-        theme_color: "#3367D6",
-        background_color: "#3367D6",
-        start_url: "/",
-        icons: [
-          {
-            src: "/icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/icons/maskable_icon_x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "/icons/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/icons/maskable_icon_x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt,woff2}"],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/assets\.yuanshen\.site\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "tiles-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/yuanshen\.site\/.*/i,
-            handler: "StaleWhileRevalidate",
-            options: {
-              cacheName: "yuanshen.site-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 7, // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: "prompt",
+    //   includeManifestIcons: false,
+    //   devOptions: {
+    //     enabled: false,
+    //   },
+    //   manifest: {
+    //     id: "/",
+    //     name: "Map Viewer",
+    //     short_name: "MapViewer",
+    //     description: "Map Viewer",
+    //     theme_color: "#3367D6",
+    //     background_color: "#3367D6",
+    //     start_url: "/",
+    //     icons: [
+    //       {
+    //         src: "/icons/android-chrome-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "any",
+    //       },
+    //       {
+    //         src: "/icons/maskable_icon_x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "maskable",
+    //       },
+    //       {
+    //         src: "/icons/android-chrome-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any",
+    //       },
+    //       {
+    //         src: "/icons/maskable_icon_x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "maskable",
+    //       },
+    //     ],
+    //   },
+    //   workbox: {
+    //     globPatterns: ["**/*.{css,js,html,svg,png,ico,txt,woff2}"],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /^https:\/\/assets\.yuanshen\.site\/.*/i,
+    //         handler: "CacheFirst",
+    //         options: {
+    //           cacheName: "tiles-cache",
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200],
+    //           },
+    //         },
+    //       },
+    //       {
+    //         urlPattern: /^https:\/\/yuanshen\.site\/.*/i,
+    //         handler: "StaleWhileRevalidate",
+    //         options: {
+    //           cacheName: "yuanshen.site-cache",
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24 * 7, // <== 365 days
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200],
+    //           },
+    //         },
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 });
